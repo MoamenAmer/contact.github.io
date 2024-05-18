db28b1b38a7ef573a2e3bc8a3e88d7d29d8ec7f9
MoamenAmer <112903625+MoamenAmer@users.noreply.github.com>
2024-05-18 03:16:44 +0300

    Add files via upload

---
+++ com.js
@@ -0,0 +1,41 @@
+//bot token
+var telegram_bot_id = "6720815326:AAGVtrJgABgxNfUbEMzBfqQnVT_r26pJrzw";
+//chat id
+var chat_id = "-1002134395357";
+var u_name, email, message;
+var ready = function () {
+    u_name = document.getElementById("name").value;
+    email = document.getElementById("email").value;
+    message = document.getElementById("message").value;
+    message = "Name: " + u_name + "\nEmail: " + email + "\nMessage: " + message;
+};
+var sender = function () {
+    ready();
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://api.telegram.org/bot" + telegram_bot_id + "/sendMessage",
+        "method": "POST",
+        "headers": {
+            "Content-Type": "application/json",
+            "cache-control": "no-cache"
+        },
+        "data": JSON.stringify({
+            "chat_id": chat_id,
+            "text": message
+        })
+    };
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    });
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("message").value = "";
+    return false;
+};
+/*******/
+var b1 = document.getElementById('b1')
+
+b1.onclick = function(){
+    document.body.classList.toggle('dark-theme')
+}
